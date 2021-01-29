d72ded9c37afafdeb09fff5184867abe4ce7110d
Tony Landschoot <tony.landschoot@outlook.com>
2021-01-29 15:29:32 +0100

    first commit

---
+++ src/main/java/com/mabiereetmoi/api/MabiereetmoiApiApplication.java
@@ -0,0 +1,13 @@
+package com.mabiereetmoi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabiereetmoiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabiereetmoiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mabiereetmoi/api/MabiereetmoiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabiereetmoi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabiereetmoiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
