9511c10534968651d959a6bd4f16266428271bd4
gabrielcongin <gabriel.congin@davidson.fr>
2021-04-02 16:45:56 +0200

    fix : script

--- src/main/java/com/mabiereetmoi/api/sql/scriptBeer.sql
+++ src/main/java/com/mabiereetmoi/api/sql/scriptBeer.sql
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-insert into category_beer values (1,'Rouge');
-insert into category_beer values (2,'Ambrée');
-insert into category_beer values (3,'Brune');
-insert into category_beer values (4,'IPA');
-insert into category_beer values (5,'Blonde');
-insert into category_beer values (6,'Brune');
-
-insert into beer(id_beer,name_beer,note_avg,degree_alcohol,description,create_date,category_id_category) values(1,'Heineken',5,5,
-'La plus connue des hollandaises est blonde pâle et mousse moyennement et de façon non persistante. Son nez d''intensité moyenne à
-                          faible est dominé par des odeurs d''orange amère',now(),5);
-
-insert into beer(id_beer,name_beer,note_avg,degree_alcohol,description,create_date,category_id_category) values(2,'Kastle Rouge',5,8,
-                                                                                                    'La Kasteel Rouge est un mélange unique de Kasteel Donker et de liqueur de cerises. Cette bière fruitée se savoure en cocktail ou apéritif sur une terrasse ensoleillée. Une bière pour les amateurs des arômes puissants et sucrés de cerises.',now(),1);
-=======
 insert into category_beer values (1,'Rouge');
 insert into category_beer values (2,'Ambrée');
 insert into category_beer values (3,'Brune');
@@ -91,4 +76,3 @@ values(12,'kwak',4,8.4,
        'Depuis sa fondation en 1791 par Evarist Bosteels, la brasserie Bosteels est devenue la définition de l''entreprise familiale. Dans la même famille depuis plus de 200 ans, la brasserie a
 été dirigée par sept générations de la famille Bosteels, brassant certaines bières les plus emblématiques de la Belgique.',
        now(),2);
->>>>>>> 91d91ff8caab9e8f5ed65e1844be9477ee05dc1a
